c884e12bcca3056258258e21ff62501db4a6e741
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-07 10:22:41 -0700

    Add files via upload

---
+++ Day 01/task.js
@@ -0,0 +1,13 @@
+// 🚀 Day 1 Challenge: Start Coding! 🚀
+// Question 1: Install Node.js, TypeScript and VS Code on your computer.
+//                         XXXXXXXXXXXXXXX
+// Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+var personName = "Tayyaba Ramzan";
+console.log("".concat(personName, ", would you like to learn some TypeScript today?"));
+//                         XXXXXXXXXXXXXXX
+// Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var myName = "Tayyaba Ramzan";
+console.log(myName.toUpperCase());
+console.log(myName.charAt(0) + myName.toUpperCase().slice(1).toLowerCase());
+console.log(myName.toLowerCase());
+//                         XXXXXXXXXXXXXXX
---
+++ Day 01/task.ts
@@ -0,0 +1,21 @@
+// 🚀 Day 1 Challenge: Start Coding! 🚀
+
+// Question 1: Install Node.js, TypeScript and VS Code on your computer.
+
+//                         XXXXXXXXXXXXXXX
+
+// Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+
+let personName:string="Tayyaba Ramzan";
+console.log(`${personName}, would you like to learn some TypeScript today?`);
+
+//                         XXXXXXXXXXXXXXX
+
+// Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let myName:string="Tayyaba Ramzan";
+console.log(myName.toUpperCase());
+console.log(myName.charAt(0)+myName.toUpperCase().slice(1).toLowerCase());
+console.log(myName.toLowerCase());
+
+//                         XXXXXXXXXXXXXXX
