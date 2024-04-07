f4d4e9a9000674df03b8ac9e5bf91edc2b9b1795
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-07 10:50:05 -0700

    Add files via upload

---
+++ Day 04/task.ts
@@ -0,0 +1,25 @@
+// 🚀 Day 4 Challenge: Start Coding! 🚀
+
+// Question 1: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does.
+
+// Tayyaba Ramzan
+// Date 7:04:2024
+
+//                         XXXXXXXXXXXXXXX
+
+// Question 2: Names: Store the names of a few of your friends in an array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let friends: string[] = ["Abeeha", "Safia", "Sumaira"];
+for (let friend of friends) {
+    console.log(friend);
+}
+
+//                         XXXXXXXXXXXXXXX
+
+// Question 3: Greetings: Use the array from Exercise 11. Instead of just printing each person’s name, print a message to them. The message should be the same for each person, but personalized with their name.
+
+let myFriends: string[] = ["Abeeha", "Safia", "Sumaira"];
+for (let myfriend of myFriends) {
+    console.log(`Hello ${myfriend}, would you like to learn some TypeScript today?`);
+}
+//                         XXXXXXXXXXXXXXX
