73db56f18cb8f535f09bcb8fd871937b9293fc3c
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-07 10:40:08 -0700

    Add files via upload

---
+++ Day 03/task.ts
@@ -0,0 +1,33 @@
+// 🚀 Day 3 Challenge: Start Coding! 🚀
+
+// Question 1: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Enclose your operations in print statements.
+
+console.log(12 + 4); //Addition
+console.log(12 - 2); //Subtraction
+console.log(16 * 2); //Multiplication
+console.log(64 / 8); //Division
+
+//                           XXXXXXXXXXXXXXXXXXX
+
+// Question 8: You should create four lines that look like this:
+// console.log(5 + 3)
+
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(16 / 2);
+
+//                           XXXXXXXXXXXXXXXXXXX
+
+// Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
+
+let myFavoriteNumber: number = 7;
+console.log(`My Favorite Number is ${myFavoriteNumber}.`);
+
+//                           XXXXXXXXXXXXXXXXXXX
+
+
+
+
+
+
