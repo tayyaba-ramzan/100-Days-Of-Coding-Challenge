3091ec6a95c64f829b75706bf71b1d6be2bcdfab
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-08 02:40:44 -0700

    Update task.js

--- Day 04/task.js
+++ Day 04/task.js
@@ -1,16 +1,16 @@
 // 🚀 Day 4 Challenge: Start Coding! 🚀
-// Question 1: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does.
+// Question 10: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does.
 // Tayyaba Ramzan
 // Date 7:04:2024
 //                         XXXXXXXXXXXXXXX
-// Question 2: Names: Store the names of a few of your friends in an array called names. Print each person’s name by accessing each element in the list, one at a time.
+// Question 11: Names: Store the names of a few of your friends in an array called names. Print each person’s name by accessing each element in the list, one at a time.
 var friends = ["Abeeha", "Safia", "Sumaira"];
 for (var _i = 0, friends_1 = friends; _i < friends_1.length; _i++) {
     var friend = friends_1[_i];
     console.log(friend);
 }
 //                         XXXXXXXXXXXXXXX
-// Question 3: Greetings: Use the array from Exercise 11. Instead of just printing each person’s name, print a message to them. The message should be the same for each person, but personalized with their name.
+// Question 12: Greetings: Use the array from Exercise 11. Instead of just printing each person’s name, print a message to them. The message should be the same for each person, but personalized with their name.
 var myFriends = ["Abeeha", "Safia", "Sumaira"];
 for (var _a = 0, myFriends_1 = myFriends; _a < myFriends_1.length; _a++) {
     var myfriend = myFriends_1[_a];
