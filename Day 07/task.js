4e96b4d332b67e176464c380abd403bceee96028
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-08 01:06:20 -0700

    Add files via upload

---
+++ Day 07/task.js
@@ -0,0 +1,17 @@
+// 🚀 Day 7 Challenge: Start Coding! 🚀
+// Question 1: Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message indicating the number of people you are inviting to dinner.
+var guests = ["Abeeha", "Fabeeha", "Faryal", "Kinza"];
+console.log("I am inviting ".concat(guests.length, " people to dinner."));
+//                           XXXXXXXXXXXXXXXXXXX
+// Question 2: Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+var countries = ["Japan", "Canada", "New Zealand", "Iceland", "Switzerland"];
+console.log("Countries I'd like to visit:", countries);
+//                           XXXXXXXXXXXXXXXXXXX
+// Question 3: Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+var book = {
+    title: "The Hobbit",
+    author: "J.R.R Tolkein",
+    yearPublished: 1937
+};
+console.log("Book Info: ".concat(book.title, " by ").concat(book.author, ", published in ").concat(book.yearPublished));
+//                           XXXXXXXXXXXXXXXXXXX
---
+++ Day 07/task.ts
@@ -0,0 +1,34 @@
+// 🚀 Day 7 Challenge: Start Coding! 🚀
+
+// Question 1: Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message indicating the number of people you are inviting to dinner.
+
+let guests:string[]=["Abeeha","Fabeeha","Faryal","Kinza"];
+console.log(`I am inviting ${guests.length} people to dinner.`);
+
+
+//                           XXXXXXXXXXXXXXXXXXX
+
+// Question 2: Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let countries: string[] = ["Japan", "Canada", "New Zealand", "Iceland", "Switzerland"];
+console.log("Countries I'd like to visit:", countries);
+
+//                           XXXXXXXXXXXXXXXXXXX
+
+// Question 3: Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let book={
+    title:"The Hobbit",
+    author:"J.R.R Tolkein",
+    yearPublished:1937
+
+}
+console.log(`Book Info: ${book.title} by ${book.author}, published in ${book.yearPublished}.`);
+
+//                           XXXXXXXXXXXXXXXXXXX
+
+
+
+
+
+
