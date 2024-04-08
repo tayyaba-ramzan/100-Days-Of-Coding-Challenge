8d5cbc291d60660527106f70d7ebaf78450302b7
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-08 06:18:04 -0700

    Add files via upload

---
+++ Day 12/task.js
@@ -0,0 +1,23 @@
+// 🚀 Day 12 Challenge: Start Coding! 🚀
+// Question 34: Pizzas: Share your favorite pizzas and express your love for them.
+var pizzas = ["pepperoni", "margherita", "hawaiian"];
+for (var _i = 0, pizzas_1 = pizzas; _i < pizzas_1.length; _i++) {
+    var pizza = pizzas_1[_i];
+    console.log("I like ".concat(pizza, " pizza."));
+}
+console.log("I really love Pizza!");
+//                               XXXXXXXXXXXXXXXX
+// Question 35: Animals: Highlight animals with a common trait.
+var animals = ["Dog", "Cat", "Rabbit"];
+for (var _a = 0, animals_1 = animals; _a < animals_1.length; _a++) {
+    var animal = animals_1[_a];
+    console.log("A ".concat(animal, " would make a great pet."));
+}
+console.log("Any of these animals would make a great pet!");
+//                               XXXXXXXXXXXXXXXX
+// Question 36: T-Shirt: Create a function for customizing t-shirts.
+function make_shirt(size, message) {
+    console.log("Making a ".concat(size, " t-shirt with the message \"").concat(message, "\" printed on it."));
+}
+make_shirt("medium", "Code is Life");
+//                               XXXXXXXXXXXXXXXX
---
+++ Day 12/task.ts
@@ -0,0 +1,33 @@
+// 🚀 Day 12 Challenge: Start Coding! 🚀
+
+// Question 34: Pizzas: Share your favorite pizzas and express your love for them.
+
+let pizzas: string[] = ["pepperoni", "margherita", "hawaiian"];
+for (let pizza of pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+
+}
+console.log("I really love Pizza!");
+
+
+//                               XXXXXXXXXXXXXXXX
+
+// Question 35: Animals: Highlight animals with a common trait.
+
+let animals: string[] = ["Dog", "Cat", "Rabbit"];
+for (let animal of animals) {
+    console.log(`A ${animal} would make a great pet.`);
+}
+console.log("Any of these animals would make a great pet!");
+
+//                               XXXXXXXXXXXXXXXX
+
+// Question 36: T-Shirt: Create a function for customizing t-shirts.
+
+function make_shirt(size: string, message: string) {
+    console.log(`Making a ${size} t-shirt with the message "${message}" printed on it.`);
+}
+
+make_shirt("medium", "Code is Life");
+
+//                               XXXXXXXXXXXXXXXX
