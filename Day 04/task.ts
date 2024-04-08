5559197fcd3941ff7aee6e2591b77771b7cac66f
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-08 02:41:08 -0700

    Update task.ts

--- Day 04/task.ts
+++ Day 04/task.ts
@@ -1,13 +1,13 @@
 // 🚀 Day 4 Challenge: Start Coding! 🚀
 
-// Question 1: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does.
+// Question 10: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does.
 
 // Tayyaba Ramzan
 // Date 7:04:2024
 
 //                         XXXXXXXXXXXXXXX
 
-// Question 2: Names: Store the names of a few of your friends in an array called names. Print each person’s name by accessing each element in the list, one at a time.
+// Question 11: Names: Store the names of a few of your friends in an array called names. Print each person’s name by accessing each element in the list, one at a time.
 
 let friends: string[] = ["Abeeha", "Safia", "Sumaira"];
 for (let friend of friends) {
@@ -16,7 +16,7 @@ for (let friend of friends) {
 
 //                         XXXXXXXXXXXXXXX
 
-// Question 3: Greetings: Use the array from Exercise 11. Instead of just printing each person’s name, print a message to them. The message should be the same for each person, but personalized with their name.
+// Question 12: Greetings: Use the array from Exercise 11. Instead of just printing each person’s name, print a message to them. The message should be the same for each person, but personalized with their name.
 
 let myFriends: string[] = ["Abeeha", "Safia", "Sumaira"];
 for (let myfriend of myFriends) {
