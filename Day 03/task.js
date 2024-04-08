5a86aa25c2fce59aa793b0a6d3de489df89baa87
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-08 02:39:06 -0700

    Update task.js

--- Day 03/task.js
+++ Day 03/task.js
@@ -1,5 +1,5 @@
 // 🚀 Day 3 Challenge: Start Coding! 🚀
-// Question 1: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Enclose your operations in print statements.
+// Question 7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Enclose your operations in print statements.
 console.log(12 + 4); //Addition
 console.log(12 - 2); //Subtraction
 console.log(16 * 2); //Multiplication
