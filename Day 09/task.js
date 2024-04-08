a692de988a831dc8dc8db994c582997a04e6ce0f
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-08 02:45:48 -0700

    Update task.js

--- Day 09/task.js
+++ Day 09/task.js
@@ -1,13 +1,5 @@
 // 🚀 Day 6 Challenge: Start Coding! 🚀
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
+
 // Question 1: More Guests: You've found a bigger dinner table, so there's room for more guests.
 var guests = ["Abeeha", "Fabeeha", "Faryal", "Kinza"];
 console.log("Great news! I found a bigger dinner table!");
