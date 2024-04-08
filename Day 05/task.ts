eee5d7e8e21a936e8f3e4ee3c6c0f49a4104d00e
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-08 02:41:49 -0700

    Update task.ts

--- Day 05/task.ts
+++ Day 05/task.ts
@@ -1,6 +1,6 @@
 // 🚀 Day 5 Challenge: Start Coding! 🚀
 
-// Question 1: Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+// Question 13: Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
 
 let transports: string[] = ["Honda", "Comic", "BikesForever"];
 for (let transport of transports) {
@@ -10,7 +10,7 @@ for (let transport of transports) {
 
 //                               XXXXXXXXXXXXXXXX
 
-// Question 2: Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+// Question 14: Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
 
 let guests: string[] = ["Amir", "Faisal", "Hassan"];
 for (let guest of guests) {
@@ -20,7 +20,7 @@ for (let guest of guests) {
 
 //                               XXXXXXXXXXXXXXXX
 
-// Question 3: Changing Guest List: One of your guests can't make it to the dinner, so you need to send out a new set of invitations with a replacement guest.
+// Question 15: Changing Guest List: One of your guests can't make it to the dinner, so you need to send out a new set of invitations with a replacement guest.
 
 let unableToAttend:string="Faisal";
 console.log(`${unableToAttend} can not make it to dinner.`);
