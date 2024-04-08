7bca95dc5e1248bd25c984bb5a7c146c5e24bb3e
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-08 02:43:52 -0700

    Update task.js

--- Day 07/task.js
+++ Day 07/task.js
@@ -1,13 +1,13 @@
 // 🚀 Day 7 Challenge: Start Coding! 🚀
-// Question 1: Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message indicating the number of people you are inviting to dinner.
+// Question 19: Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message indicating the number of people you are inviting to dinner.
 var guests = ["Abeeha", "Fabeeha", "Faryal", "Kinza"];
 console.log("I am inviting ".concat(guests.length, " people to dinner."));
 //                           XXXXXXXXXXXXXXXXXXX
-// Question 2: Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+// Question 20: Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
 var countries = ["Japan", "Canada", "New Zealand", "Iceland", "Switzerland"];
 console.log("Countries I'd like to visit:", countries);
 //                           XXXXXXXXXXXXXXXXXXX
-// Question 3: Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+// Question 21: Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
 var book = {
     title: "The Hobbit",
     author: "J.R.R Tolkein",
