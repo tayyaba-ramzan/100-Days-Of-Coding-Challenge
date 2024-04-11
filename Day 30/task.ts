373681c2525914d9b8b012c201a75fa149426c37
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-11 11:11:20 -0700

    Add files via upload

---
+++ Day 30/task.ts
@@ -0,0 +1,29 @@
+// 🚀 Day 30 Challenge: Start Coding! 🚀
+
+// Question 88: Rounding to the Nearest Integer: Write a function that takes a decimal number as input and returns the number rounded to the nearest integer.
+
+function roundToNearestInteger(num:number){
+    return Math.round(num);
+}
+console.log(roundToNearestInteger(7.7));
+
+//                         XXXXXXXXXXXXXXX
+
+// Question 89: Converting Strings to Numbers: Create a function that takes a string representing a number (like "123") and converts it into an actual number type.
+
+function convertStringToNumber(str:string):number {
+    return parseFloat(str);
+}
+console.log(convertStringToNumber("99"));
+
+//                         XXXXXXXXXXXXXXX
+
+// Question 90: Checking if a Value is NaN: Demonstrate how to check if a variable's value is NaN (Not a Number) and return a boolean result.
+
+function isValueNan(value:any):boolean {
+    return isNaN(value);
+}
+console.log(isValueNan("Bye"));
+console.log(isValueNan(102030));
+
+//                         XXXXXXXXXXXXXXX
