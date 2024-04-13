0d8e2aca67f3a4b697e34dc7eb22b4d65d3aa177
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-12 21:29:41 -0700

    Add files via upload

---
+++ Day 34/task.ts
@@ -0,0 +1,24 @@
+// 🚀 Day 34 Challenge: Start Coding! 🚀
+
+// Question 100: Use the JavaScript Math object to find the square root of 144.
+
+const squareRoot:number=Math.sqrt(144);
+console.log(squareRoot);
+
+//                         XXXXXXXXXXXXXXX
+
+// Question 101: Generate a random integer between 1 and 10.
+
+function getRandomNumber():number {
+    return Math.floor(Math.random()*10)+1;
+}
+console.log(getRandomNumber());
+
+//                         XXXXXXXXXXXXXXX
+
+// Question 102: Calculate and log the absolute difference between the number -5 and 5.
+
+const difference:number=Math.abs(-10-5);
+console.log(difference);
+
+//                         XXXXXXXXXXXXXXX
