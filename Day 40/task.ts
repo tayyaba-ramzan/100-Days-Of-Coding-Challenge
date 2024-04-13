8e3896db259c84283eb99702df4f63ca496b3f24
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-13 10:13:41 -0700

    Add files via upload

---
+++ Day 40/task.ts
@@ -0,0 +1,30 @@
+// 🚀 Day 40 Challenge: Start Coding! 🚀
+
+// Question 118: Write a loop that logs numbers from 1 to 10 to the console.
+
+for (let i=1;i<=10;i++){
+  console.log(i);
+  
+}
+
+//                              XXXXXXXXXXXXXXXX
+
+// Question 119: Create a while loop that logs "Hello, World!" 5 times.
+
+let j=1;
+while (j<=5) {
+  console.log("Hello World");
+  j++;
+}
+
+//                               XXXXXXXXXXXXXXXX
+
+// Question 120: Use a for...of loop to iterate through an array of your favorite movies and log each one.
+
+let favoriteMovies:string[]=["Bin Roye","Dil Waley","Wrong NO"];
+for (let favoriteMovie of favoriteMovies){
+  console.log(favoriteMovie);
+  
+}
+
+//                               XXXXXXXXXXXXXXXX
