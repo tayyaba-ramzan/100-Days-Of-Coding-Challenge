30d698544b2003332921c44a572d1b06085dfa48
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-14 20:33:42 -0700

    Add files via upload

---
+++ Day 45/task.ts
@@ -0,0 +1,34 @@
+// 🚀 Day 45 Challenge: Start Coding! 🚀
+
+// Question 133: Write a JavaScript object and convert it into a JSON string.
+
+const person = {
+    name: "Tayyaba",
+    age: 20,
+    city: "Karachi"
+}
+const jsonString = JSON.stringify(person);
+console.log(jsonString);
+
+//                              XXXXXXXXXXXXXXXX
+
+
+// Question 134: Take a JSON string and parse it into a JavaScript object.
+
+// const jsonString = '{"name":"Tayyaba","age":20,"city":"Karachi"}';
+// const person = JSON.parse(jsonString);
+// console.log(person);
+
+//                               XXXXXXXXXXXXXXXX
+
+// Question 135: Explain how you can format a JSON string with proper indentation for readability.
+
+// const person = {
+//     name: "Tayyaba",
+//     age: 20,
+//     city: "Karachi"
+// }
+// const jsonString = JSON.stringify(person, null, 2);
+// console.log(jsonString);
+
+//                               XXXXXXXXXXXXXXXX
