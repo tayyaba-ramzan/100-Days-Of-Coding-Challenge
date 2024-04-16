ca153311bf62252072c4f50f5d3200cc351290c1
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-15 20:59:08 -0700

    Update task.ts

--- Day 01/task.ts
+++ Day 01/task.ts
@@ -1,6 +1,7 @@
 // 🚀 Day 1 Challenge: Start Coding! 🚀
 
 // Question 1: Install Node.js, TypeScript and VS Code on your computer.
+//                             DONE
 
 //                         XXXXXXXXXXXXXXX
 
