d9289d6a1ea46ded966185e76afaa6ec0682917e
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-21 22:21:12 -0700

    Update task.ts

--- Day 37/task.ts
+++ Day 37/task.ts
@@ -3,7 +3,7 @@
 // Question 109: Write an if statement that logs "Good Morning" if the current time is before 12 PM.
 
 const currentTime = new Date();
-if (currentTime.getHours() < 12) {
+if (currentTime.getHours() > 12) {
   console.log("Good Morning"); 
 }
 
