588eddbfb53eed5955052d13194e15f5479164c4
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-23 06:10:44 -0700

    Update task.ts

--- Day 47/task.ts
+++ Day 47/task.ts
@@ -15,7 +15,7 @@ function add(a,b) {
 //                              XXXXXXXXXXXXXXXX
 
 // Question 140: Explain the error that occurs when trying to use a reserved word as a variable name.
-
+// let if = 5;
 console.log(
     "Using a reserved word as a variable name causes a syntax error in JavaScript."
   );
