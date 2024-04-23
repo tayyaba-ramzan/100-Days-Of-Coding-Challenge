0105c4b7f77dd72e6671f4187ad80dc53ffa0f7e
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-23 06:06:42 -0700

    Add files via upload

---
+++ Day 47/task.ts
@@ -0,0 +1,36 @@
+// 🚀 Day 46 Challenge: Start Coding! 🚀
+
+// Question 139: List three reserved words in JavaScript and create a valid use case for each.
+
+let count:number=12;
+if (count>0) {
+    console.log("Count is greater than 0.");
+    
+}
+function add(a,b) {
+    return a+b;
+    
+}
+
+//                              XXXXXXXXXXXXXXXX
+
+// Question 140: Explain the error that occurs when trying to use a reserved word as a variable name.
+
+console.log(
+    "Using a reserved word as a variable name causes a syntax error in JavaScript."
+  );
+
+//                               XXXXXXXXXXXXXXXX
+
+// Question 141: Discuss the significance of the await reserved word in asynchronous JavaScript.
+
+async function fetchSomething() {
+    const data=await fetchSomething();
+    console.log(data);
+    
+}
+console.log(
+    "The 'await' keyword allows asynchronous, promise-based behavior to be written in a cleaner, more linear fashion."
+  );
+
+//                               XXXXXXXXXXXXXXXX
