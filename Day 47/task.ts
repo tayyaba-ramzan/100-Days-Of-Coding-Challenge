60d31ae838116b8ae379ba54cf0f6fbe91ab7326
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-23 06:22:29 -0700

    Update task.ts

--- Day 47/task.ts
+++ Day 47/task.ts
@@ -2,11 +2,16 @@
 
 // Question 139: List three reserved words in JavaScript and create a valid use case for each.
 
+// Reversed Word Let
 let count:number=12;
+
+// Reversed Word If
 if (count>0) {
     console.log("Count is greater than 0.");
     
 }
+
+// Reversed Word Function
 function add(a,b) {
     return a+b;
     
