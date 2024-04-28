9d418c9e07aef54ee7666194fcd8e93ed4ad8513
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:16:25 -0700

    Update task.ts

--- Day 22/task.ts
+++ Day 22/task.ts
@@ -1,3 +1,5 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+
 // 🚀 Day 22 Challenge: Start Coding! 🚀
 
 // Question 64: Combine Strings and Numbers: Write a function that merges a piece of text with a number. For example, if provided with "Age: " and 30, it should give back "Age: 30".
