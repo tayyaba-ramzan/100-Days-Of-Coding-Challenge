75a6a1b4ded17592977ff4fa06c20edca988b986
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:08:04 -0700

    Update task.js

--- Day 05/task.js
+++ Day 05/task.js
@@ -1,3 +1,4 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
 // 🚀 Day 5 Challenge: Start Coding! 🚀
 // Question 13: Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
 var transports = ["Honda", "Comic", "BikesForever"];
