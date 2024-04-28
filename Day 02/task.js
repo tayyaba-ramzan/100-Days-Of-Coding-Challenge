cced625abd2acd1039af1a32006cdf33f752010c
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:05:04 -0700

    Update task.js

--- Day 02/task.js
+++ Day 02/task.js
@@ -1,3 +1,4 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
 // 🚀 Day 2 Challenge: Start Coding! 🚀
 // Question 4: Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
 // "Albert Einstein once said, “A person who never made a mistake never tried anything new.”"
