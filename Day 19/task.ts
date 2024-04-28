3c3e31dc1e94a38f5219f9c8903a08490dd901c1
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:14:39 -0700

    Update task.ts

--- Day 19/task.ts
+++ Day 19/task.ts
@@ -1,3 +1,5 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+
 // 🚀 Day 19 Challenge: Start Coding! 🚀
 
 // Question 55: Double Numbers in an Array: Make a list of numbers. Then, use a trick to make a new list where each number is twice its original value.
