974214d19eafee1e473704d3acb83c9159ee8ffa
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 01:22:00 -0700

    Delete Day 51 directory

--- Day 51/index.ts
+++
@@ -1,8 +0,0 @@
-//                 🚀|| 100 Days of Coding Challenge || 🚀
-
-
-// Day-52 Task:
-// Read the following article:
-// https://javascript.info/intro
-
-//                              XXXXXXXXXXXXXXXX
