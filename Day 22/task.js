5ba10a60fe98a3e77b3c533ad056560317c1eb2c
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:16:10 -0700

    Update task.js

--- Day 22/task.js
+++ Day 22/task.js
@@ -1,3 +1,4 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
 // 🚀 Day 22 Challenge: Start Coding! 🚀
 // Question 64: Combine Strings and Numbers: Write a function that merges a piece of text with a number. For example, if provided with "Age: " and 30, it should give back "Age: 30".
 function combineStringAndNumber(text, number) {
