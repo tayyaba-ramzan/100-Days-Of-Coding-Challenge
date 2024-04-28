fa2a7eb4e4ade3f84c11c739afeda195b311b952
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:14:26 -0700

    Update task.js

--- Day 18/task.js
+++ Day 18/task.js
@@ -1,3 +1,4 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
 // 🚀 Day 18 Challenge: Start Coding! 🚀
 // Question 52: Make a Smartphone Object: Create a simple way to keep track of a smartphone's details. Include its brand, model, and other key features like how much storage it has, the size of its screen, and how long its battery lasts.
 // Sets up details about a smartphone
