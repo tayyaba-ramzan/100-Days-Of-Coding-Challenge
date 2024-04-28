8b5966fec050dc92e1c39035724172c5f008ae03
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:18:01 -0700

    Update task.ts

--- Day 25/task.ts
+++ Day 25/task.ts
@@ -1,3 +1,5 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+
 // 🚀 Day 25 Challenge: Start Coding! 🚀
 
 // Question 73: Assigning and Updating Variables: Create a function where you declare a variable using let and assign an initial value. Then, update its value within the same function and log both the initial and updated values.
