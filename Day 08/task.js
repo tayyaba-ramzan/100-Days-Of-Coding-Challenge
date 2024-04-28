1f72d3d5e654bdb50916b7f4e849be0c42ca5593
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:09:42 -0700

    Update task.js

--- Day 08/task.js
+++ Day 08/task.js
@@ -1,3 +1,4 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
 // 🚀 Day 8 Challenge: Start Coding! 🚀
 // Question 22: Intentional Error: Try to produce an array index error in one of your programs. Correct the error before finishing.
 var cousins = ["Areeba", "Rida Fatima", "Hifza"];
