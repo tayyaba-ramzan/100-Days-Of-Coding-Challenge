c6c04b5bef5fd10255dcc641cb05434dd22b229a
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:09:12 -0700

    Update task.js

--- Day 07/task.js
+++ Day 07/task.js
@@ -1,3 +1,4 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
 // 🚀 Day 7 Challenge: Start Coding! 🚀
 // Question 19: Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message indicating the number of people you are inviting to dinner.
 var guests = ["Abeeha", "Fabeeha", "Faryal", "Kinza"];
