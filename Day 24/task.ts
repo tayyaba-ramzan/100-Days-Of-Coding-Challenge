f5332782b0173b3c0c7d7b3e273980c7a48d1947
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:17:30 -0700

    Update task.ts

--- Day 24/task.ts
+++ Day 24/task.ts
@@ -1,3 +1,5 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+
 // 🚀 Day 24 Challenge: Start Coding! 🚀
 
 // Question 70: Understanding let in Loops: Write a function that uses a loop with the let keyword to print numbers from 1 to 5. Explain how the let keyword affects the visibility of the loop variable.
