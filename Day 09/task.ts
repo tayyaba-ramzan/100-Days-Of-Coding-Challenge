8d26521f2530784287799aec303e65d548d7a708
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:09:56 -0700

    Update task.ts

--- Day 09/task.ts
+++ Day 09/task.ts
@@ -1,3 +1,5 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+
 // 🚀 Day 9 Challenge: Start Coding! 🚀
 
 // Question 25: Alien Colors #1: Imagine an alien was just shot down in a game. Assign 'green', 'yellow', or 'red' to a variable called alien_color.
