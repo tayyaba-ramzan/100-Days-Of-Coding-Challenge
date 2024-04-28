6ff91615012189f33e0e12c4b27baae52cf00a38
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:18:53 -0700

    Update task.js

--- Day 27/task.js
+++ Day 27/task.js
@@ -1,3 +1,4 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
 // 🚀 Day 27 Challenge: Start Coding! 🚀
 // Question 79: Creating and Accessing Object Properties: Construct an object representing a car with properties for make, model, and year. Then, show how you can access the model property of the car.
 var car = {
