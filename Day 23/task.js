f64c3be61a157947d42c89fc45d9ffa791d836e7
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:16:39 -0700

    Update task.js

--- Day 23/task.js
+++ Day 23/task.js
@@ -1,3 +1,4 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
 // 🚀 Day 23 Challenge: Start Coding! 🚀
 // Question 67: Arithmetic with Mixed Types: Write a function that takes two parameters: a number and a string that represents a number (e.g., "5"). Return their sum as a number. 
 function addNumberAndString(number, numberString) {
