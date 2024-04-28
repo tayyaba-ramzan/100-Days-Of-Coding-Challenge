8bfb8de338b91ab425e1bb273895148250ae323a
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:04:30 -0700

    Update task.js

--- Day 01/task.js
+++ Day 01/task.js
@@ -1,3 +1,4 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
 // 🚀 Day 1 Challenge: Start Coding! 🚀
 // Question 1: Install Node.js, TypeScript and VS Code on your computer.
 //                         XXXXXXXXXXXXXXX
