264aade4a045fa8ed2ae1b8b5ec6b9ea891de217
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:06:59 -0700

    Update task.js

--- Day 03/task.js
+++ Day 03/task.js
@@ -1,3 +1,4 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
 // 🚀 Day 3 Challenge: Start Coding! 🚀
 // Question 7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Enclose your operations in print statements.
 console.log(12 + 4); //Addition
