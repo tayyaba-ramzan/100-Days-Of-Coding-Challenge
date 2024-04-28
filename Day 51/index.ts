36f8b0d710bf434ab96e276c0d266d213dbc4cb0
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-27 21:18:09 -0700

    Add files via upload

---
+++ Day 51/index.ts
@@ -0,0 +1,8 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+
+
+// Day-52 Task:
+// Read the following article:
+// https://javascript.info/intro
+
+//                              XXXXXXXXXXXXXXXX
