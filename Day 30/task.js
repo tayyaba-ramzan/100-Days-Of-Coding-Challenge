183bab86891ee474ba39475b2ffbc3be743c0e8d
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 09:20:23 -0700

    Update task.js

--- Day 30/task.js
+++ Day 30/task.js
@@ -1,3 +1,4 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
 // 🚀 Day 30 Challenge: Start Coding! 🚀
 // Question 88: Rounding to the Nearest Integer: Write a function that takes a decimal number as input and returns the number rounded to the nearest integer.
 function roundToNearestInteger(num) {
