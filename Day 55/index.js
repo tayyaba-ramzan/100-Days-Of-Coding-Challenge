acbfc5722f4af752b80ffa183b74a2bfe1606e93
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 20:00:46 -0700

    Add files via upload

---
+++ Day 55/index.js
@@ -0,0 +1,61 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+
+// 🚀 Day 55 Challenge: Start Coding! 🚀
+
+// Datatypes in JavaScript
+
+// Primitive DataTypes
+// Number
+let age = 20
+console.log(age);
+
+//                           XXXXXXXXXXXXXXXXX
+
+// string
+let firstName = "Tayyaba";
+let lastName = "Ramzan";
+console.log(`${firstName} ${lastName}`);
+
+//                           XXXXXXXXXXXXXXXXX
+
+// Boolean
+let isRaining = false;
+console.log(isRaining);
+
+//                           XXXXXXXXXXXXXXXXX
+
+// undefined
+let undef
+console.log(undef);
+
+//                           XXXXXXXXXXXXXXXXX
+
+// Null
+let x = null
+console.log(x);
+
+//                           XXXXXXXXXXXXXXXXX
+
+// BigInt
+let y = BigInt("102030");
+console.log(y);
+
+//                           XXXXXXXXXXXXXXXXX
+
+// Symbol
+let z = Symbol("Hello!");
+console.log(z);
+
+//                           XXXXXXXXXXXXXXXXX
+
+// Non Primitive DataTypes
+// Object
+
+const student = {
+    name: "Tayyaba Ramzan",
+    age: 20,
+    email: "tayyabaramzan.pk@gmail.com",
+}
+console.log(student);
+
+//                           XXXXXXXXXXXXXXXXX
