c413e3514d3bd36e8a50a4ae084508735935c832
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-28 19:16:48 -0700

    Add files via upload

---
+++ Day 54/index.js
@@ -0,0 +1,10 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+
+// 🚀 Day 54 Challenge: Start Coding! 🚀
+
+// Hello World
+console.log("Hello World!");
+
+// Variables
+let myName="Tayyaba Ramzan";
+console.log(myName);
