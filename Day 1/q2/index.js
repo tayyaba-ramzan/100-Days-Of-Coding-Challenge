c1889533cbd431f915965787274561d6438297c9
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-06-18 12:12:59 -0700

    Update index.js

--- Day 1/q2/index.js
+++ Day 1/q2/index.js
@@ -1,4 +1,5 @@
 //                 🚀|| 100 Days of Coding Challenge || 🚀
+// 🚀 Day 1 Challenge: Start Coding! 🚀
 // Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Tayyaba Ramzan, would you like to learn some TypeScript today?”
 var personName = "Tayyaba Ramzan";
 console.log("".concat(personName, ", would you like to learn some TypeScript today?"));
