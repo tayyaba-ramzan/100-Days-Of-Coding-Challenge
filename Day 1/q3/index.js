5f6f098c229c132d002bd3ee234525ba0f415a8e
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-06-18 12:13:41 -0700

    Update index.js

--- Day 1/q3/index.js
+++ Day 1/q3/index.js
@@ -1,4 +1,5 @@
 //                 🚀|| 100 Days of Coding Challenge || 🚀
+// 🚀 Day 1 Challenge: Start Coding! 🚀
 // Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
 var myName = "Tayyaba Ramzan";
 console.log(myName.toUpperCase());
