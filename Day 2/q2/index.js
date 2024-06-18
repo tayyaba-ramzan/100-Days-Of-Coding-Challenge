226fdfa9ca5e86f35fa0737c6137d9522d0d7694
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-06-18 12:22:30 -0700

    Add files via upload

---
+++ Day 2/q2/index.js
@@ -0,0 +1,7 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+// 🚀 Day 2 Challenge: Start Coding! 🚀
+// Question 5: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+var famousPersonName = "Tayyaba Ramzan";
+var message = "Hello ".concat(famousPersonName, ", would you like to learn some TypeScript today?");
+console.log(message);
+//                               XXXXXXXXXXXXXXXX
---
+++ Day 2/q3/index.ts
@@ -0,0 +1,11 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+
+// 🚀 Day 2 Challenge: Start Coding! 🚀
+
+// Question 6: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
+
+let myName: string = "\t\nTayyaba Ramzan\t\n";
+console.log(myName);
+console.log(myName.trim());
+
+//                               XXXXXXXXXXXXXXXX
