43f1f56f3807e416b3e8b9844a2a6dfe1fae8850
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-06-26 01:48:49 -0700

    Delete Day 19 directory

--- Day 19/q55/index.js
+++
@@ -1,7 +0,0 @@
-//                 🚀|| 100 Days of Coding Challenge || 🚀
-// 🚀 Day 19 Challenge: Start Coding! 🚀
-// Question 55: Double Numbers in an Array: Make a list of numbers. Then, use a trick to make a new list where each number is twice its original value.
-var number = [2, 4, 6, 8, 10, 12];
-var doubleNumber = number.map(function (number) { return number * 2; });
-console.log(doubleNumber);
-//                         XXXXXXXXXXXXXXX
