38af9a9f7fd8802b371f7cdb3a9db6b8865961f9
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-06-26 01:49:26 -0700

    Add files via upload

---
+++ Day 19/q57/index.js
@@ -0,0 +1,7 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+// 🚀 Day 19 Challenge: Start Coding! 🚀
+// Question 57: Find the Average Grade: Given a list of grades, calculate the average grade.
+var grades = [60, 76, 88, 70, 69, 91, 53];
+var averageGrade = grades.reduce(function (total, grade) { return total + grade; }, 0) / grades.length;
+console.log(averageGrade);
+//                               XXXXXXXXXXXXXXXX
---
+++ Day 19/q57/index.ts
@@ -0,0 +1,11 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+
+// 🚀 Day 19 Challenge: Start Coding! 🚀
+
+// Question 57: Find the Average Grade: Given a list of grades, calculate the average grade.
+
+let grades:number[]=[60,76,88,70,69,91,53];
+let averageGrade=grades.reduce((total,grade)=>total+grade,0)/grades.length;
+console.log(averageGrade);
+
+//                               XXXXXXXXXXXXXXXX
