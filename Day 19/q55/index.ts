1cac008ae0fd2a331d5e224f48f0ffb530d2889e
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-06-26 01:48:36 -0700

    Add files via upload

---
+++ Day 19/q55/index.ts
@@ -0,0 +1,11 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+
+// 🚀 Day 19 Challenge: Start Coding! 🚀
+
+// Question 55: Double Numbers in an Array: Make a list of numbers. Then, use a trick to make a new list where each number is twice its original value.
+
+let number: number[] = [2, 4, 6, 8, 10, 12];
+let doubleNumber = number.map(number => number * 2);
+console.log(doubleNumber);
+
+//                         XXXXXXXXXXXXXXX
---
+++ Day 19/q56/index.js
@@ -0,0 +1,7 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+// 🚀 Day 19 Challenge: Start Coding! 🚀
+// Question 56: Keep Only Strings: Given a mix of different types of items, make a new list that has only the words.
+var mixedArray = [1, "Apple", "Banana", 2, true, false, "Carrot"];
+var stringArray = mixedArray.filter(function (item) { return typeof item === "string"; });
+console.log(stringArray);
+//                         XXXXXXXXXXXXXXX
