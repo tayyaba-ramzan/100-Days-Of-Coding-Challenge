08204682391dbff98be06cb26e6ae550ecae8a67
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-06-26 01:45:26 -0700

    Add files via upload

---
+++ Day 18/q53/index.js
@@ -0,0 +1,11 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+// 🚀 Day 18 Challenge: Start Coding! 🚀
+// Question 53: Pulling Apart a Nested Object: Imagine you have a list inside another list that shows what a computer programmer knows, like coding languages, tools, and software frameworks. Find a way to get three specific skills from this list and show them.
+var developerSkills = {
+    languages: ["JavaScript", "TypeScript", "Python"],
+    frameworks: ["React", "MongoDB", "MVC"],
+    tools: ["Git", "Webpack", "Docker"]
+};
+var languages = developerSkills.languages, frameworks = developerSkills.frameworks, tools = developerSkills.tools;
+console.log("Language: ".concat(languages[1], ", Framework: ").concat(frameworks[1], ", Tool: ").concat(tools[0]));
+//                         XXXXXXXXXXXXXXX
