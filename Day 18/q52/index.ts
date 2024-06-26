36809eea040790e384daf9df0ebbbd6e8f166cda
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-06-26 01:43:31 -0700

    Add files via upload

---
+++ Day 18/q52/index.ts
@@ -0,0 +1,21 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+
+// 🚀 Day 18 Challenge: Start Coding! 🚀
+
+// Question 52: Make a Smartphone Object: Create a simple way to keep track of a smartphone's details. Include its brand, model, and other key features like how much storage it has, the size of its screen, and how long its battery lasts.
+
+// Sets up details about a smartphone
+let smartphone = {
+    make: "Oppo",
+    model: "Oppo A78 5G",
+    specs: {
+        storage: "12GB",
+        screenSize: "7.2 inches",
+        batteryLife: "20 hours"
+    }
+};
+
+console.log(smartphone);
+console.log(smartphone.specs);
+
+//                         XXXXXXXXXXXXXXX
---
+++ Day 18/q54/index.ts
@@ -0,0 +1,17 @@
+//                 🚀|| 100 Days of Coding Challenge || 🚀
+
+// 🚀 Day 18 Challenge: Start Coding! 🚀
+
+// Question 54: Making Flexible Object Keys: Learn how to set up a list where you can change the name of each section based on what you need at that moment, like adjusting labels based on user choices.
+
+function createObjectWithDynamicKey(key: string, value: string) {
+    let dynamicObject = {};
+    dynamicObject[key] = value;
+    return dynamicObject;
+}
+
+let userPreference = createObjectWithDynamicKey("theme", "dark");
+
+console.log(userPreference);
+
+//                               XXXXXXXXXXXXXXXX
