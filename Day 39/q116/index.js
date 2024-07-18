a24787e0fcab4577cbbb0e858148b44a5abae880
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-07-18 16:36:37 -0500

    Add files via upload

---
+++ Day 39/q116/index.js
@@ -0,0 +1,32 @@
+//                 🚀 || 100 Days of Coding Challenge || 🚀
+// 🚀 Day 39 Challenge: Start Coding! 🚀
+// Question 116: Create a switch case that matches several cases to the same code block, representing seasons.
+function logSeason(month) {
+    switch (month) {
+        case 12:
+        case 1:
+        case 2:
+            console.log("Winter");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            console.log("Spring");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            console.log("Summer");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            console.log("Fall");
+            break;
+        default:
+            console.log("Invalid month");
+            break;
+    }
+}
+logSeason(11);
+//                         XXXXXXXXXXXXXXX
---
+++ Day 39/q117/index.js
@@ -0,0 +1,27 @@
+//                 🚀 || 100 Days of Coding Challenge || 🚀
+// 🚀 Day 39 Challenge: Start Coding! 🚀
+// Question 117: Implement a switch statement that evaluates an expression and uses the default case if none of the cases match.
+function evaluateGrade(grade) {
+    switch (grade) {
+        case "A":
+            console.log("Excellent");
+            break;
+        case "B":
+            console.log("Good");
+            break;
+        case "C":
+            console.log("Fair");
+            break;
+        case "D":
+            console.log("Poor");
+            break;
+        case "F":
+            console.log("Fail");
+            break;
+        default:
+            console.log("Invalid grade");
+            break;
+    }
+}
+evaluateGrade("A");
+//                         XXXXXXXXXXXXXXX
