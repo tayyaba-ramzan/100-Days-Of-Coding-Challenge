d3d776073caa0387d4b8d4e9b86d89af70debb1e
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-07-18 16:17:19 -0500

    Add files via upload

---
+++ Day 35/q103/index.ts
@@ -0,0 +1,11 @@
+//                 🚀 || 100 Days of Coding Challenge || 🚀
+
+// 🚀 Day 35 Challenge: Start Coding! 🚀
+
+// Question 103: Write a function that returns a random boolean value, true or false.
+
+function getRandomBoolean(): boolean {
+    return Math.random() > 0.5;
+}
+console.log(getRandomBoolean());
+//                              XXXXXXXXXXXXXXXX
