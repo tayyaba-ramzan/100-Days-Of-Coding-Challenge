868947b409e495c30cd03ca5018a18b2203cece4
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-07-23 13:44:29 -0700

    Delete Day 44 directory

--- Day 44/calculator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var calculator = /** @class */ (function () {
-    function calculator() {
-    }
-    return calculator;
-}());
-exports.default = calculator;
