6f0d86dbd5d52058c0e222194fd59a0441fc9107
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-07-23 13:13:57 -0700

    Delete Day 41 directory

--- Day 41/q122/index.js
+++
@@ -1,9 +0,0 @@
-//                 🚀 || 100 Days of Coding Challenge || 🚀
-// 🚀 Day 41 Challenge: Start Coding! 🚀
-// Question 122: Use a while loop to count down from 10 to 1 and breaks the loop when it reaches 5.
-var j = 10;
-while (j > 0) {
-    console.log(j);
-    j--;
-}
-//                         XXXXXXXXXXXXXXX
