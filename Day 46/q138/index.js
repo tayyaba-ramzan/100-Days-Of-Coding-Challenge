3222cd39b204bf835334faeb26b5670536417475
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-07-29 14:22:37 -0500

    Add files via upload

---
+++ Day 46/q138/index.js
@@ -0,0 +1,7 @@
+//                 🚀 || 100 Days of Coding Challenge || 🚀
+// 🚀 Day 46 Challenge: Start Coding! 🚀
+// Question 138: Describe how to use breakpoints in browser developer tools to debug JavaScript code.
+console.log("Before breakpoint");
+console.log("This line has a breakpoint set on it in the browser's developer tools");
+console.log("After breakpoint");
+//                         XXXXXXXXXXXXXXX
